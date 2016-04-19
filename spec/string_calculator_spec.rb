1564a9e9eadad497ad473b545cd045fcb7d44bbc
LegaTux <josmpena91@gmail.com>
2016-04-19 19:06:53 +0200

    Add test to return single digit number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,5 +7,15 @@ describe StringCalculator do
         expect(StringCalculator.add("")).to eql(0)
       end
     end
+
+    context "given a single number as string" do
+      context "'5'" do
+        it "returns 5" do
+          expect(StringCalculator.add("5")).to eql(5)
+        end
+      end
+
+    #   context "'10'"
+    end
   end
 end
